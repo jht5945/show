cca188de9654feca90a24254222ff31a7089a736
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-09-01 18:53:19 +0800

    V -> version, v -> verbose

--- src/opt.rs
+++ src/opt.rs
@@ -24,8 +24,8 @@ impl Options {
     pub fn parse_args(&mut self) {
         let mut ap = ArgumentParser::new();
         ap.set_description("show - command line tool.");
-        ap.refer(&mut self.version).add_option(&["-v", "--version"], StoreTrue, "Print version");
-        ap.refer(&mut self.verbose).add_option(&["-V", "--verbose"], StoreTrue, "Verbose print");
+        ap.refer(&mut self.version).add_option(&["-V", "--version"], StoreTrue, "Print version");
+        ap.refer(&mut self.verbose).add_option(&["-v", "--verbose"], StoreTrue, "Verbose print");
         ap.refer(&mut self.cmd).add_argument("CMD", Store, "Command, use ':::' show all");
         ap.parse_args_or_exit();
     }
